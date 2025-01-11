22e471a8dac8701bf83a362993bcf3f3d8ce8352
Adam Abdillah <adamabdillah20@gmail.com>
2025-01-11 12:29:52 +0700

    fix order status

--- models/Tag.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const TagSchema = new mongoose.Schema({
-  name: { type: String, required: true }
-});
-
-const Tag = mongoose.model('Tag', CategorySchema);
-
-module.exports = Category;
